0d84aff3b45324e5b49593f453001f8c7d0dde7d
Caleb Land <caleb@land.fm>
2015-04-06 12:02:06 -0400

    Install curl before trying to use it

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tar czf helpers-2015-04-04.tar.gz helpers
+tar czf helpers-v1.0.tar.gz helpers
 open .
 open https://github.com/caleb/docker-helpers/releases
--- helpers/install-base.sh
+++ helpers/install-base.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+SYSLOG_FORWARDER_VERSION=1.0
+
+#
+# Install runit and monit
+#
+apt-get update
+apt-get install -y monit runit curl
+rm -rf /var/lib/apt/lists/*
+
 #
 # Install mo, the bash mustache templating engine
 #
@@ -8,14 +17,7 @@ curl https://raw.githubusercontent.com/caleb/mo/master/mo > /usr/local/bin/mo
 #
 # Install syslog_forwarder
 #
-curl https://github.com/caleb/syslog_forwarder/releases/download/v$SYSLOG_FORWARDER_VERSION/syslog_forwarder-linux-amd64-v$SYSLOG_FORWARDER_VERSION.tar.gz > /tmp/syslog_forwarder.tar.gz
+curl -L https://github.com/caleb/syslog_forwarder/releases/download/v${SYSLOG_FORWARDER_VERSION}/syslog_forwarder-linux-amd64-v${SYSLOG_FORWARDER_VERSION}.tar.gz > /tmp/syslog_forwarder.tar.gz
 
 tar xzf /tmp/syslog_forwarder.tar.gz -C /usr/local/bin
 rm /tmp/syslog_forwarder.tar.gz
-
-#
-# Install runit and monit
-#
-apt-get update
-apt-get install -y monit runit
-rm -rf /var/lib/apt/lists/*
